ae5b5fa6de6f65995a89e7e23079525813bc4da6
leon.dav.acosta <leon.dav.acosta@gmail.com>
2022-06-12 17:30:33 -0300

    Add project files.

---
+++ tp1/tp1.cpp
@@ -0,0 +1,27 @@
+//librerias 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//constantes
+const int MAX_REGS = 800;
+const int MAX_VENS = 100;
+
+//struct
+struct ventasVendedores {
+    int id,
+
+};
+
+int main()
+{
+    ofstream myFile("VentasFerreteria.txt");
+}
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
