a771ac1286e66d5ace115e3e4472b422a7347bab
leon.dav.acosta <leon.dav.acosta@gmail.com>
2022-06-12 18:13:35 -0300

    init

--- tp1/tp1.cpp
+++ tp1/tp1.cpp
@@ -1,16 +1,16 @@
-//librerias 
+// librerias
 #include <iostream>
 #include <fstream>
 using namespace std;
 
-//constantes
+// constantes
 const int MAX_REGS = 800;
 const int MAX_VENS = 100;
 
-//struct
-struct ventasVendedores {
-    int id,
-
+// struct
+struct ventasVendedores
+{
+    int id;
 };
 
 int main()
@@ -18,7 +18,7 @@ int main()
     ofstream myFile("VentasFerreteria.txt");
 }
 
-// Tips for Getting Started: 
+// Tips for Getting Started:
 //   1. Use the Solution Explorer window to add/manage files
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
